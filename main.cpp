4f612da4c3de91caa4f055478c94d4ef9405cb37
Brian Rajchel <60751431+bmrajchel@users.noreply.github.com>
2020-11-16 15:30:40 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
@@ -14,16 +17,22 @@ void printMemory(void);
 int freeFrameList [100];
 int freeFrameIndex = 0;
 int processList [100][2];
+int pageTable [100][2];
+int *freeFrame;
+int PTOffset = 0;
+int pListOffset = 0;
 
 int main()
 {
     bool run = true;
-    int memSize, frameSize, size, pid, addr1, ret;
+    int memSize, frameSize, sizeA, pid, addr1, ret;
     string str;
 
     for (int i = 0; i < 100; i++)
     {
         freeFrameList[i] = -1;
+        pageTable[i][0] = -1;
+        pageTable [i][1] = -1;
     }
 
     do
@@ -45,21 +54,33 @@ int main()
             }
             else if (str[0] == 'A')
             {
-                cin >> size;
+                cin >> sizeA;
                 cin >> pid;
-                ret = allocate(size, pid);
+                ret = allocate(sizeA, pid);
+                if(ret==-1)
+                {
+                    cout<<"Error in allocation.\n";
+                }
             }
             else if (str[0] == 'W')
             {
                 cin >> pid;
                 cin >> addr1;
                 ret = write(pid, addr1);
+                if(ret==-1)
+                {
+                    cout<<"No such page assigned to that process.\n";
+                }
             }
             else if (str[0] == 'R')
             {
                 cin >> pid;
                 cin >> addr1;
                 ret = read(pid, addr1);
+                if(ret==-1)
+                {
+                    cout<<"No such page assigned to that process.\n";
+                }
             }
             else if (str[0] == 'D')
             {
@@ -77,10 +98,9 @@ int main()
 
 void memoryManager (int memSize, int frameSize)
 {
-    //cout << "MEM" << endl << memSize << endl << frameSize << endl;
-    int *freeFrame;
+    //creates freeframse of size passed by user
     freeFrame = new int [memSize];
-
+    //sets all freeframes to 0 and sets the index of the frames in free frames list
     for (int i = 0; i < memSize; i++)
     {
         freeFrame[i] = 0;
@@ -92,42 +112,132 @@ void memoryManager (int memSize, int frameSize)
 int allocate (int allocSize, int pid)
 {
     //cout << "ALLOC" << endl << allocSize << endl << pid<< endl;;
+    srand(time(NULL));
+    int randNum, runs, index;
+    bool notFound = true;
+    bool error = false;
+    randNum=rand()% freeFrameIndex;
+    runs = 0;
+    for(int i = 0;i<allocSize;i++)//For the full allocation
+    {
+        pageTable[i+PTOffset][0] = pid;//adds to the page table
+        while(notFound)//searches for a random free frame
+        {
+            randNum=rand()% freeFrameIndex;
+            if(freeFrameList[randNum]!=-1)
+            {
+                notFound = false;
+                index  = freeFrameList[randNum];
+                freeFrameList[randNum] = -1;
+            }
+            runs++;
+            if(runs>1000)//if there aren't any free frames
+            {
+                notFound = false;
+                error = true;
+            }
+        }
+        runs = 0;
+        notFound = true;
+        if(error)//stops if there is an error
+        {
+          return -1;
+        }
+        pageTable[i+PTOffset][1] = index;
+    }
+    PTOffset += allocSize;
+    processList[pListOffset][0]=pid;
+    processList[pListOffset][1]=allocSize;
+    pListOffset++;
+    return 1;
 }
 
 int deallocate (int pid)
 {
     //cout << "DEALLOC" << endl << pid << endl;
+    for(int i = 0;i<100;i++)//Goes through the page table
+    {
+        if(pageTable[i][0]==pid)
+        {
+            for(int j = 0;j<100;j++)
+            {
+                if(freeFrameList[j]==-1)//Finds the first available free frame
+                {
+                    freeFrameList[j]=pageTable[i][1];
+                    j=1000;
+                }
+            }
+            pageTable[i][0]=-1;//clears page table
+            pageTable[i][1]=-1;
+        }
+    }
+    return 1;
 }
 
 int write (int pid, int logical_address)
 {
     //cout << "WRITE" <<  endl << pid << endl << logical_address << endl;
+    int pageCount = 0;
+    for(int i = 0;i<100;i++)//Finds the correct page
+    {
+        if(pageTable[i][0]==pid)
+        {
+            if(pageTable[i][0]==pid&&pageCount==logical_address)
+            {
+                freeFrame[pageTable[i][1]] = 1;//Writes to the correct page
+                return 1;
+            }
+            pageCount++;
+        }
+    }
+    return -1;
 }
 
 int read (int pid, int logical_address)
 {
     //cout << "READ" <<  endl << pid << endl << logical_address << endl;
+    int pageCount = 0;
+    for(int i = 0;i<100;i++)//Finds correct page
+    {
+        if(pageTable[i][0]==pid)
+        {
+            if(pageTable[i][0]==pid&&pageCount==logical_address)
+            {
+                cout<<freeFrame[pageTable[i][1]]<<endl;//Prints contents
+                return 1;
+            }
+            pageCount++;
+        }
+    }
+    return -1;
 }
 
 void printMemory(void)
 {
     //free
+    //display free frames
+    cout << "Free" << endl;
     for (int i = 0; i < 100; i ++)
     {
         if (freeFrameList[i] != -1)
         {
             cout << i << " ";
-            cout << freeFrameList[i] << endl;
         }
     }
     cout << endl << endl;
-    //not free
-        for (int i = 0; i < 100; i ++)
+
+    //process
+    //display process size and pid
+    cout << "Process" << endl;
+    for (int i = 0; i < 100; i++)
     {
-        if (freeFrameList[i] == -1)
+        if (processList[i][0] > 0)
         {
-            cout << i << " ";
+            cout << processList[i][0] << " ";
+            cout << processList[i][1];
+            cout << endl;
         }
     }
-    cout << endl;
+    cout << endl << endl;
+
 }
